2d3bccd0ff3b7fd3b6a6411f231cc04536be0119
Shakil <shakil.uddin2001@gmail.com>
2022-02-17 09:53:01 +0000

    Player Movement Added. Jumping and Walking

--- KOTJ-Personal/King_Of_The_Jungle/Assets/Scripts/Player/PlayerMovement.cs
+++ KOTJ-Personal/King_Of_The_Jungle/Assets/Scripts/Player/PlayerMovement.cs
@@ -4,15 +4,45 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private Rigidbody2D body;
+    [SerializeField] private float speed;
+    [SerializeField] private float jumpPower;
+    [SerializeField] int maxJumps;
+    private int jumpCount;
+
+    private void Awake() //Called Everytime script is loaded
     {
-        
+        body = GetComponent<Rigidbody2D>(); //Gets component from game object from inspector tab
+        jumpCount = maxJumps;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update() //Runs every frame
     {
-        
+
+        Debug.Log(jumpCount);
+        float horizontalInput = Input.GetAxis("Horizontal"); //Store horizontal Input (-1, 0 ,1)
+
+        body.velocity = new Vector2(horizontalInput * speed, body.velocity.y);
+
+        //Flip player when changing direction
+        if (horizontalInput > 0.01f)
+            transform.localScale = Vector3.one;
+        else if (horizontalInput < -0.01f)
+            transform.localScale = new Vector3(-1, 1, 1);
+      
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            if (jumpCount > 0)
+                body.velocity = new Vector2(body.velocity.x, jumpPower);
+                jumpCount -= 1;
+        }
     }
+
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Ground")
+            jumpCount = maxJumps;
+    }
+
 }
