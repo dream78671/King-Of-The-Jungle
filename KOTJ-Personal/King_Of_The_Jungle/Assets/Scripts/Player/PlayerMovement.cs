c2bdae0d0fced8d6fed941108d5a402fcf72877a
Shakil <shakil.uddin2001@gmail.com>
2022-02-17 10:31:55 +0000

    Animation Added for Idle, running, jumping

--- KOTJ-Personal/King_Of_The_Jungle/Assets/Scripts/Player/PlayerMovement.cs
+++ KOTJ-Personal/King_Of_The_Jungle/Assets/Scripts/Player/PlayerMovement.cs
@@ -5,14 +5,17 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     private Rigidbody2D body;
+    private Animator anim; 
     [SerializeField] private float speed;
     [SerializeField] private float jumpPower;
     [SerializeField] int maxJumps;
     private int jumpCount;
+    private bool grounded; 
 
     private void Awake() //Called Everytime script is loaded
     {
         body = GetComponent<Rigidbody2D>(); //Gets component from game object from inspector tab
+        anim = GetComponent<Animator>(); 
         jumpCount = maxJumps;
     }
 
@@ -29,20 +32,34 @@ public class PlayerMovement : MonoBehaviour
             transform.localScale = Vector3.one;
         else if (horizontalInput < -0.01f)
             transform.localScale = new Vector3(-1, 1, 1);
-      
+
+
+        //Jump - GetKeyDown used to only register the initial click, not holding the space bar
         if (Input.GetKeyDown(KeyCode.Space))
         {
             if (jumpCount > 0)
-                body.velocity = new Vector2(body.velocity.x, jumpPower);
-                jumpCount -= 1;
+                Jump();
         }
+
+        //If character is moving, turn run animation on, else turn it off
+        anim.SetBool("Run", horizontalInput != 0);
+        anim.SetBool("Grounded", grounded);
     }
 
+    private void Jump()
+    {
+        body.velocity = new Vector2(body.velocity.x, jumpPower);
+        anim.SetTrigger("Jump");
+        jumpCount -= 1;
+        grounded = false; 
+    }
 
     void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.tag == "Ground")
+            grounded = true;
             jumpCount = maxJumps;
+     
     }
 
 }
