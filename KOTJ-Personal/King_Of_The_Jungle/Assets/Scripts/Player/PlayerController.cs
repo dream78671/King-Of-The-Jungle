233d21d0fef7c784edc1d9752e932f913a5b6cfc
Shakil <shakil.uddin2001@gmail.com>
2022-03-12 15:23:29 +0000

    Player Controller split into 2 scripts. PlayerMovement and PLayerController

--- KOTJ-Personal/King_Of_The_Jungle/Assets/Scripts/Player/PlayerController.cs
+++ KOTJ-Personal/King_Of_The_Jungle/Assets/Scripts/Player/PlayerController.cs
@@ -12,15 +12,9 @@ public class PlayerController : MonoBehaviourPunCallbacks, IOnEventCallback, IDa
     RaiseEventOptions raiseEventOptions = new RaiseEventOptions { Receivers = ReceiverGroup.All }; //Send event to all clients
 
     private Rigidbody2D body;
-    private Animator anim;
     private PhotonView PV;
 
-    [SerializeField] private float speed;
-    [SerializeField] private float jumpPower;
-    [SerializeField] int maxJumps;
     [SerializeField] Slider slider; 
-    private int jumpCount;
-    private bool grounded;
     
     public bool canMove = false;
     public bool canShoot = false; 
@@ -45,10 +39,7 @@ public class PlayerController : MonoBehaviourPunCallbacks, IOnEventCallback, IDa
     private void Awake() //Called Everytime script is loaded
     {
         body = GetComponent<Rigidbody2D>(); //Gets component from game object from inspector tab
-        anim = GetComponent<Animator>();
         PV = GetComponent<PhotonView>();
-        body.freezeRotation = true;
-        jumpCount = maxJumps;
         gameObject.tag = "Player";
 
         if (PV.IsMine && PhotonNetwork.IsMasterClient)
@@ -77,44 +68,8 @@ public class PlayerController : MonoBehaviourPunCallbacks, IOnEventCallback, IDa
             return;
         }
         
-        float horizontalInput = Input.GetAxis("Horizontal"); //Store horizontal Input (-1, 0 ,1)
-
-        body.velocity = new Vector2(horizontalInput * speed, body.velocity.y);
-
-        //Flip player when changing direction
-        if (horizontalInput > 0.01f)
-            transform.localScale = Vector3.one;
-        else if (horizontalInput < -0.01f)
-            transform.localScale = new Vector3(-1, 1, 1);
-
-        //Jump - GetKeyDown used to only register the initial click, not holding the space bar
-        if (Input.GetKeyDown(KeyCode.Space))
-        {
-            if (jumpCount > 0)
-                Jump();
-        }
-
-        //If character is moving, turn run animation on, else turn it off
-        anim.SetBool("Run", horizontalInput != 0);
-        anim.SetBool("Grounded", grounded);
     }
 
-    private void Jump()
-    {
-        body.velocity = new Vector2(body.velocity.x, jumpPower);
-        anim.SetTrigger("Jump");
-        jumpCount -= 1;
-        grounded = false;
-
-    }
-
-    //When player collides with Ground, reset number of jumps
-    void OnCollisionEnter2D(Collision2D collision)
-    {
-        if (collision.gameObject.tag == "Ground")
-            grounded = true;
-        jumpCount = maxJumps;
-    }
 
     //Photon - Deals with events that are called
     public void OnEvent(EventData photonEvent)
---
+++ KOTJ-Personal/King_Of_The_Jungle/Assets/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,76 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+
+public class PlayerMovement : MonoBehaviour
+{
+    private Rigidbody2D body;
+    private Animator anim;
+    private PhotonView PV;
+    private PlayerController Player; 
+
+    [SerializeField] private float speed;
+    [SerializeField] private float jumpPower;
+    [SerializeField] int maxJumps;
+    private int jumpCount;
+    private bool grounded;
+
+
+    private void Awake()
+    {
+        body = GetComponent<Rigidbody2D>(); //Gets component from game object from inspector tab
+        anim = GetComponent<Animator>();
+        PV = GetComponent<PhotonView>();
+        Player = GetComponent<PlayerController>();
+        body.freezeRotation = true;
+        jumpCount = maxJumps;
+    }
+
+    private void Update()
+    {
+        if (!Player.canMove)
+            return;
+
+        if (!PV.IsMine)
+            return;
+
+        float horizontalInput = Input.GetAxis("Horizontal"); //Store horizontal Input (-1, 0 ,1)
+
+        body.velocity = new Vector2(horizontalInput * speed, body.velocity.y);
+
+        //Flip player when changing direction
+        if (horizontalInput > 0.01f)
+            transform.localScale = Vector3.one;
+        else if (horizontalInput < -0.01f)
+            transform.localScale = new Vector3(-1, 1, 1);
+
+        //Jump - GetKeyDown used to only register the initial click, not holding the space bar
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            if (jumpCount > 0)
+                Jump();
+        }
+
+        //If character is moving, turn run animation on, else turn it off
+        anim.SetBool("Run", horizontalInput != 0);
+        anim.SetBool("Grounded", grounded);
+    }
+
+    private void Jump()
+    {
+        body.velocity = new Vector2(body.velocity.x, jumpPower);
+        anim.SetTrigger("Jump");
+        jumpCount -= 1;
+        grounded = false;
+
+    }
+
+    //When player collides with Ground, reset number of jumps
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Ground")
+            grounded = true;
+        jumpCount = maxJumps;
+    }
+}
